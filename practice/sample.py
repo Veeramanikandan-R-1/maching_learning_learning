3b2ea5a5192de0e16c3326855ce6bfb944a44bc8
mieupro <Veeramanikandan R>
2021-08-03 14:42:26 +0530

    first commit

---
+++ practice/sample.py
@@ -0,0 +1,6 @@
+import pandas as pd
+df=pd.read_csv('music.csv')
+# print(df.shape) #to get no of rows and columns
+# print(df.describe()) #to get info of each column and dataset gives basics statics about data
+# print(df.values) # to get values in array
+print(df)
