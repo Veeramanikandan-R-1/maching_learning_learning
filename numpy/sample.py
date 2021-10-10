44e1a48a0f728d0787bc7397e5576b2ad3f2fad1
Veeramanikandan-R-1 <r.veeramanikandany216@gmail.com>
2021-10-11 05:13:57 +0530

    real time sale tb

--- numpy/sample.py
+++ numpy/sample.py
@@ -1,12 +1,23 @@
-import numpy as np
-array1=np.array([1,2,3,3,4,5,6,6])
-filtered_array=[]
-for i in array1:
-    if(i>4):
-        filtered_array.append(True)
-    else:
-        filtered_array.append(False)
-new_arr=array1[filtered_array]
-print(new_arr)
-print(filtered_array)
-
+grades=[73,67,38,39]
+print(grades)
+for grade in grades:
+    def rounding(val):
+        actual_value = grade
+        remainder_val = actual_value % 10
+        if(remainder_val == (0 or 5)):
+            rounded_value = actual_value
+        elif(1 <= remainder_val < 5):
+            rounded_value = (actual_value - remainder_val) + 5
+        elif(5 < remainder_val <= 10):
+            rounded_value = actual_value + (10 - remainder_val)
+        return rounded_value
+    round_val=rounding(grade)
+    print(round_val)
+    if (grade < 38):
+        continue
+    elif((round_val-grade) < 3):
+        print(round_val - grade)
+        grade1 =round_val
+        grades.insert((grades.index(grade)),grade1)
+        grades.remove(grade)
+print(grades)
